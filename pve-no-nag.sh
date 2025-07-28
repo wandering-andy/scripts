4f560020b9efe6490284329df9cd7baf34aaa8e8
Andy Jones <3579815+wandering-andy@users.noreply.github.com>
2025-07-28 17:26:05 -0600

    Update pve-no-nag.sh

--- pve-no-nag.sh
+++ pve-no-nag.sh
@@ -1,7 +1,3 @@
-#!/usr/bin/env bash
+wget https://free-pmx.pages.dev/tools/free-pmx-no-subscription_0.3.0~pre1.deb -O /tmp/free-pmx.deb
 
-echo "Disabling subscription nag"
-
-echo "DPkg::Post-Invoke { \"if [ -s /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js ] && ! grep -q -F 'NoMoreNagging' /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js; then echo 'Removing subscription nag from UI...'; sed -i '/data\.status/{s/\!//;s/active/NoMoreNagging/}' /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js; fi\" };" >/etc/apt/apt.conf.d/no-nag-script
-
-echo "Disabled subscription nag (Delete browser cache)"
+apt install /tmp/free-pmx.deb
